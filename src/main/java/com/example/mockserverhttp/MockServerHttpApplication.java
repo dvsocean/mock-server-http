6a8814a53ee2b3d0e7adb4e6a7b33f473f00c152
Ocean <dvsocean@icloud.com>
2018-06-29 12:05:04 -0700

    layout to confirgure project in gradle

---
+++ src/main/java/com/example/mockserverhttp/MockServerHttpApplication.java
@@ -0,0 +1,12 @@
+package com.example.mockserverhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerHttpApplication.class, args);
+	}
+}
