3717fd528fb76419d3dfc92917c5fe11cca5c200
= <dvsocean@icloud.com>
2018-07-01 12:21:20 -0700

    added another user who extends from base user

--- src/test/java/com/example/testRunner/ActionsForFrontPage.java
+++ src/test/java/com/example/testRunner/ActionsForFrontPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.example.managers.InstanceManager;
 import com.example.selectors.DesktopRunner;
+import com.example.users.Danika;
 import com.example.users.User;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
@@ -14,7 +15,7 @@ public class ActionsForFrontPage {
 
     private String url = "http://www.mtsac.edu/";
 
-    private User user = new User();
+    private Danika danika = new Danika();
 
     @DataProvider(name = "source1")
     public Object[][] dataSourceOne() {
@@ -32,12 +33,12 @@ public class ActionsForFrontPage {
 
    @Test(dataProvider = "source1")
     public void checkScheduleForFall(InstanceManager instanceManager){
-        instanceManager.onTheFrontPage().loginToSchoolPortalFor(user);
+        instanceManager.onTheFrontPage().loginToSchoolPortalFor(danika);
     }
 
     @Test(dataProvider = "source1")
     public void findDetailsForObtainingDegree(InstanceManager instanceManager){
-        instanceManager.onTheFrontPage().find(user);
+        instanceManager.onTheFrontPage().find(danika);
     }
 
     @AfterMethod
---
+++ src/test/java/com/example/users/Danika.java
@@ -0,0 +1,6 @@
+package com.example.users;
+
+public class Danika extends User {
+
+
+}
